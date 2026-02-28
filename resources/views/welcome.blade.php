d7708f0698c6d6dd6b1497a136f2f70a023fd3ed
Putranugroho14 <putranugroho61@gmail.com>
2026-02-28 12:43:37 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased bg-black min-h-screen flex items-center justify-center p-4">
+
+    <!-- Main Container -->
+    <div class="w-full max-w-5xl border border-zinc-800 bg-[#111111] rounded-lg p-16 shadow-2xl">
+        <div class="text-left">
+            <!-- Name -->
+            <h1 class="text-white font-bold text-xl mb-1">Putra Nugroho</h1>
+
+            <!-- NIM -->
+            <p class="text-zinc-400 text-sm mb-8">NIM : 20230140165</p>
+
+            <!-- Button -->
+            <ul class="flex gap-3 text-sm leading-normal">
+                <li>
+                    <a href="https://github.com/code-worker-me/PWF-Genap-2025/blob/main/pertemuan-1.md" target="_blank"
+                        class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                        Modul Pertemuan 1
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+
+</html>
